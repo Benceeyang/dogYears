4816a545f5e4a95b8424e3ac6b95c3c0fffe07c4
Benceeyang <104700515+Benceeyang@users.noreply.github.com>
2022-05-03 19:42:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let myAge = 22;
+let earlyYears = 2;
+let earlYears = earlyYears * 10.5;
+let laterYears = myAge - 2;
+//^This is my age subtracted from 2
+
+let laterYears = laterYears * 4;
+
+let myAgeInDogYears= ('earlyYears' + 'laterYears');
+
+let myName= 'Bencee'.toLowerCase();
+
+console.log(My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years in dog years);
+
+
+
+
