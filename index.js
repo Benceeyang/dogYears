96e529833de059419dd16094a5c807465d11cf89
Bencee Yang <byang@icstars.org>
2022-05-03 20:04:32 -0500

    Code works, Math doesn't

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let earlYears = earlyYears * 10.5;
 let laterYears = myAge - 2;
 //^This is my age subtracted from 2
 
-let laterYears = laterYears * 4;
+let laterLife = laterYears * 4;
 
-let myAgeInDogYears= ('earlyYears' + 'laterYears');
+let myAgeInDogYears= (earlyYears + laterLife);
 
 let myName= 'Bencee'.toLowerCase();
 
-console.log(My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years in dog years);
+console.log(`My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
 
 
 
